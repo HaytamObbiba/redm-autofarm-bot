fac3a000020603dafdc544563e0bac8397223358
HaytamObbiba <155924142+HaytamObbiba@users.noreply.github.com>
2026-09-10 06:04:53 +0100

    Add fxmanifest configuration

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'rdr3'
+
+author 'Your Name'
+description 'RedM Autofarm Bot - Standalone Mining Bot'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html'
+}
